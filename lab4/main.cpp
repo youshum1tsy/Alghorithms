f674d98811186a30d0976bf26db7b91fd40cdf2e
youshum1tsy <balaev.gleb@mail.ru>
2024-10-06 13:29:59 +0300

    lab 4, task 2
    Реализовать функцию подсчёта числа вхождений заданного элемента в дерево.

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
+
 struct Node {
 	Node* right = nullptr;
 	Node* left = nullptr;
 	int data = 0;
 };
-
 struct Node* CreateTree(struct Node* root, struct Node* r, int data)
 {
 	if (r == NULL)
@@ -15,15 +15,13 @@ struct Node* CreateTree(struct Node* root, struct Node* r, int data)
 			std::cout << "Memmory error";
 			exit(0);
 		}
-
 		r->left = NULL;
 		r->right = NULL;
 		r->data = data;
-
 		if (root == NULL) {
 			return r;
 		}
-		if (data > root->data)	{
+		if (data > root->data) {
 			root->left = r;
 		}
 		else {
@@ -31,45 +29,36 @@ struct Node* CreateTree(struct Node* root, struct Node* r, int data)
 		}
 		return r;
 	}
-
 	if (data > r->data) {
 		CreateTree(r, r->left, data);
 	}
 	else {
 		CreateTree(r, r->right, data);
 	}
-
 	return root;
 }
-
 void print_tree(struct Node* r, int l)
 {
 	if (r == NULL)
 	{
 		return;
 	}
-
 	print_tree(r->right, l + 1);
 	for (int i = 0; i < l; i++)
 	{
 		std::cout << " ";
 		std::cout << " ";
 	}
-
 	std::cout << r->data << "\n";
 	print_tree(r->left, l + 1);
 }
-
 struct Node* find_node(struct Node* root, int find_data) {
-
 	if (root == NULL) {
 		return nullptr;
 	}
-
 	if (root->data == find_data) {
 		return root;
 	}
-
 	if (find_data > root->data) {
 		return find_node(root->left, find_data);
 	}
@@ -78,20 +67,44 @@ struct Node* find_node(struct Node* root, int find_data) {
 	}
 }
 
+int count_elements_in_tree(struct Node* root, int target) {
+	
+	if (root == NULL) {
+		return 0;
+	}
+	int count;
+	if (root->data == target) {
+		count = 1;
+	}
+	else {
+		count = 0;
+	}
+
+	count += count_elements_in_tree(root->right, target);
+	count += count_elements_in_tree(root->left, target);
+	
+	return count;
+}
+
+void separator() {
+	std::cout << "-----\n";
+}
+
 int main()
 {
 	setlocale(LC_ALL, "");
 	int D, start = 1;
 	int find = 0;
-	Node *root = NULL;
-	std::cout << "-1 - окончание построения дерева\n";
+	int target = 0;
+	Node* root = NULL;
+	std::cout << "enter -1 to end of the tree construction\n";
 	while (start)
 	{
-		std::cout << "Введите число: ";
+		std::cout << "enter value of the tree element: ";
 		std::cin >> D;
 		if (D == -1)
 		{
-			std::cout << "Построение дерева окончено\n\n";
+			std::cout << " ";
 			start = 0;
 		}
 		else {
@@ -99,10 +112,14 @@ int main()
 		}
 	}
 
+	separator();
 	print_tree(root, 0);
-	
+	separator();
+
 	std::cout << "enter find value: ";
 	std::cin >> find;
+	separator();
+
 	Node* findNode = find_node(root, find);
 	if (findNode != nullptr) {
 		print_tree(findNode, 0);
@@ -110,6 +127,12 @@ int main()
 	else {
 		std::cout << "element not found";
 	}
+
+	separator();
+	std::cout << "enter target to count in tree: ";
+	std::cin >> target;
+	std::cout << "count: " << count_elements_in_tree(root, target) << "\n";
+	separator();
 
 	return 0;
 }
